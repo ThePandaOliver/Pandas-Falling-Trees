0f46a279935864c7c07885e5f6a2b1776386fdaf
The Panda Oliver <Oliver.h.froberg@gmail.com>
2025-10-31 09:38:41 +0100

    Downgrade Minecraft version to 1.21.3 and update related dependencies in gradle.properties; modify RenderUtils for block rendering

--- src/main/kotlin/utils/RenderUtils.kt
+++ src/main/kotlin/utils/RenderUtils.kt
@@ -14,12 +14,15 @@ package dev.pandasystems.fallingtrees.utils
 
 import com.mojang.blaze3d.vertex.PoseStack
 import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer
 import net.minecraft.client.renderer.ItemBlockRenderTypes
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.texture.OverlayTexture
 import net.minecraft.core.BlockPos
 import net.minecraft.world.item.ItemDisplayContext
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.BlockAndTintGetter
+import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.state.BlockState
 
 object RenderUtils {
@@ -28,27 +31,30 @@ object RenderUtils {
 		poseStack: PoseStack, blockState: BlockState, blockPos: BlockPos,
 		level: BlockAndTintGetter, bufferSource: MultiBufferSource, packedLight: Int
 	) {
-		val packedOverlay = OverlayTexture.NO_OVERLAY
 		val blockRenderDispatcher = Minecraft.getInstance().blockRenderer
+		val blockEntityRenderDispatcher: BlockEntityWithoutLevelRenderer = blockRenderDispatcher.blockEntityRenderer
+		val renderShape = blockState.renderShape
+		when (renderShape) {
+			RenderShape.MODEL -> {
+				val bakedModel = blockRenderDispatcher.getBlockModel(blockState)
+				val color = blockRenderDispatcher.blockColors.getColor(blockState, level, blockPos, 0)
+				val red = (color shr 16 and 0xFF).toFloat() / 255.0f
+				val green = (color shr 8 and 0xFF).toFloat() / 255.0f
+				val blue = (color and 0xFF).toFloat() / 255.0f
+				blockRenderDispatcher.modelRenderer.renderModel(
+					poseStack.last(),
+					bufferSource.getBuffer(ItemBlockRenderTypes.getRenderType(blockState)), blockState,
+					bakedModel, red, green, blue, packedLight, OverlayTexture.NO_OVERLAY
+				)
+			}
 
-		val bakedModel = blockRenderDispatcher.getBlockModel(blockState)
-		val color = blockRenderDispatcher.blockColors.getColor(blockState, level, blockPos, 0)
-		val red = (color shr 16 and 255).toFloat() / 255.0f
-		val green = (color shr 8 and 255).toFloat() / 255.0f
-		val blue = (color and 255).toFloat() / 255.0f
-		blockRenderDispatcher.modelRenderer
-			.renderModel(
-				poseStack.last(),
-				bufferSource.getBuffer(ItemBlockRenderTypes.getRenderType(blockState)),
-				blockState,
-				bakedModel,
-				red,
-				green,
-				blue,
-				packedLight,
-				packedOverlay
+			RenderShape.ENTITYBLOCK_ANIMATED -> blockEntityRenderDispatcher.renderByItem(
+				ItemStack(blockState.block),
+				ItemDisplayContext.NONE,
+				poseStack, bufferSource, packedLight, OverlayTexture.NO_OVERLAY
 			)
-		blockRenderDispatcher.specialBlockModelRenderer.get()
-			.renderByBlock(blockState.block, ItemDisplayContext.NONE, poseStack, bufferSource, packedLight, packedOverlay)
+
+			else -> {}
+		}
 	}
 }
