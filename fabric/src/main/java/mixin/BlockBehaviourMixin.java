aa7125da6c08e90c076684bf8e5401db0b2042fb
The Panda Oliver <oliver.froberg@proton.me>
2025-12-20 18:57:38 +0100

    Refactor mixin implementations: extract logic to Kotlin implementation classes

--- fabric/src/main/java/mixin/BlockBehaviourMixin.java
+++ fabric/src/main/java/mixin/BlockBehaviourMixin.java
@@ -14,12 +14,14 @@ package dev.pandasystems.fallingtrees.fabric.mixin;
 
 import dev.pandasystems.fallingtrees.api.TreeHandler;
 import dev.pandasystems.fallingtrees.config.FallingTreesConfigKt;
+import dev.pandasystems.fallingtrees.mixin.BlockBehaviourKtImpl;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -28,11 +30,11 @@ import java.util.Optional;
 
 @Mixin(BlockBehaviour.class)
 public class BlockBehaviourMixin {
+	@Unique
+	private BlockBehaviourKtImpl fallingtrees$impl =  new BlockBehaviourKtImpl();
+
 	@Inject(method = "getDestroyProgress", at = @At("RETURN"), cancellable = true)
 	private void getDestroyProgress(BlockState state, Player player, BlockGetter level, BlockPos pos, CallbackInfoReturnable<Float> cir) {
-		if (FallingTreesConfigKt.getFallingTreesCommonConfig().get().getDynamicMiningSpeed().getDisable()) return;
-		if (player == null || !TreeHandler.canPlayerChopTree(player)) return;
-		Optional<Float> miningSpeedOpt = TreeHandler.getMiningSpeed(player, pos, cir.getReturnValue());
-		miningSpeedOpt.ifPresent(cir::setReturnValue);
+		fallingtrees$impl.getDestroyProgress(player, pos, cir);
 	}
 }
